ced7372709a4376f23ed0d3280a0b57da110203f
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2025-01-27 19:00:25 +0530

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,100 @@
+#keyword list
+import keyword
+print(keyword.kwlist)
+
+print(len(keyword.kwlist))
+
+# variable define
+a = 7
+print(a)
+santhu, podili, = 15, 23
+print(santhu,podili)
+a = 45
+b = 55
+print("c is:", a+b)
+
+#work with intiger
+i = 2
+print(i)
+hmm = type(i)
+print(hmm)
+#work with float
+f = 2.5
+print(f)
+tmm = type(f)
+print(tmm)
+f3 = 3e2
+print(f3)
+#work with bool
+b = True
+print(b)
+b1 = False
+print(b1)
+b2 = None
+print(b2)
+ttt = type(b)
+print(ttt)
+tf = True + True + False - True
+print(tf)
+ff = True * False
+print(ff)
+# complex work
+c = 20+4j
+print(c)
+print(type(c))
+ss = c.real
+print(ss)
+ffff = c.imag
+print(ffff)
+c1 = 5+5j
+print(c+c1)
+ok = c.conjugate
+print(ok)
+print(c-c1)
+print(c*c1)
+c2 = 10+2.5j
+print(c2) 
+c3 = 2.0+34j
+print(c3)
+# work with string
+s = "genrative ai & llm model great career ahead. also it is research domain"
+print(s)
+s = 'genrative ai & llm model great career ahead. also it is research domain'
+print(s)
+s = """genrative ai & llm model great career ahead. also it is research domain"""
+print(s)
+s = '''genrative ai & llm model great career ahead. also it is research domain'''
+print(s)
+
+#work with type casting
+gandu = int(2.3)
+print(gandu)
+dan = int(True)
+omm = int("10")
+print(dan)
+print(omm)
+ss = float(10)
+print(ss)
+fffff = float(True)
+print(fffff)
+aa = float('20')
+print(aa)
+print(bool(9))
+print(bool(9.9))
+print(bool('9'))
+print(bool(9+9j))
+print(bool())
+
+ggg = str(2)
+print(ggg)
+fffff = str(2.2)
+print(fffff)
+sasa = str(True)
+print(sasa)
+dd = str(10+10j)
+print(dd)
+com = 'milk'
+print(com[0])
+print(com[1])
+print(com[2])
+print(com[3])
